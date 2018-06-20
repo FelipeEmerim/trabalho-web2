b91dd50d42ce03b74fb8840b537f2cba875d43b8
FelipeEmerim <lip.emerim@gmail.com>
2018-06-20 00:02:00 -0300

    corrigida validação usando regex no php, corrigido validação de email repetido no lado cliente, trocado o método de get para post nas requisições de cadastro.html

--- cadastro.php
+++ cadastro.php
@@ -1,17 +1,17 @@
 <?php
     $pdo = new PDO('mysql:host=localhost;dbname=loja_virtual', 'root', '');
 
-    switch($_GET['action']){
+    switch($_POST['action']){
         case 'getUser':
             $comando = $pdo->prepare("SELECT count(*) AS cont FROM cadastros WHERE email = ?");
-            $comando->execute(array($_GET['email']));
+            $comando->execute(array($_POST['email']));
             $data = $comando->fetch(PDO::FETCH_ASSOC);
             echo json_encode($data);
             break;
 
         case 'cadastro':
             $comando = $pdo->prepare("SELECT count(*) AS cont FROM cadastros WHERE email = ?");
-            $comando->execute(array($_GET['email']));
+            $comando->execute(array($_POST['email']));
             $resultado = $comando->fetch(PDO::FETCH_ASSOC);
             if($resultado['cont'] >= 1){
                 $data = ['succeso'=> false, 'msg'=>'O usuário já existe'];
@@ -19,27 +19,27 @@
                 exit();
             }
 
-            $nome = $_GET['nome'];
+            $nome = $_POST['nome'];
             $nome = trim($nome);
             $s_nome = filter_var($nome, FILTER_SANITIZE_STRING);
             if(!filter_var($s_nome,FILTER_VALIDATE_REGEXP, array(
-                "options"=>array("regexp"=>"/[a-zA-Z ]{3,}/")))){
+                "options"=>array("regexp"=>"/^[a-zA-Z ]{3,}$/")))){
                 $data= ['sucesso'=>false, 'msg'=>'Nome de usuário invalido'];
                 echo json_encode($data);
                 exit();
             }
 
-            $endereco = $_GET['endereco'];
+            $endereco = $_POST['endereco'];
             $endereco = trim($endereco);
             $s_endereco = filter_var($endereco, FILTER_SANITIZE_STRING);
-            if(!filter_var($s_nome,FILTER_VALIDATE_REGEXP, array(
-                "options"=>array("regexp"=>"/[a-zA-Z0-9\, ]{10,}/")))){
+            if(!filter_var($s_endereco,FILTER_VALIDATE_REGEXP, array(
+                "options"=>array("regexp"=>"/^[a-zA-Z0-9, ]{10,}$/")))){
                 $data= ['sucesso'=>false, 'msg'=>'endereco invalido'];
                 echo json_encode($data);
                 exit();
             }
 
-            $date = $_GET['data'];
+            $date = $_POST['data'];
             $s_date = trim($date);
             if (!filter_var($s_date, FILTER_VALIDATE_REGEXP, array(
                 "options"=>array("regexp"=>"/\d{2}\/\d{2}\/\d{4}/")))){
@@ -48,7 +48,7 @@
                 exit();
             }
 
-            $sexo = $_GET['sexo'];
+            $sexo = $_POST['sexo'];
             $s_sexo = trim($sexo);
             if (!filter_var($s_sexo, FILTER_VALIDATE_REGEXP, array(
                 "options"=>array("regexp"=>"/(?:masculino|feminino)/")))){
@@ -57,7 +57,7 @@
                 exit();
             }
 
-            $email = $_GET['email'];
+            $email = $_POST['email'];
             $email = trim($email);
             $s_email = filter_var($email, FILTER_SANITIZE_EMAIL);
             if(!filter_var($s_email, FILTER_VALIDATE_EMAIL)){
@@ -66,12 +66,12 @@
                 exit();
             }
 
-            $senha = $_GET['senha'];
+            $senha = $_POST['senha'];
             $senha = trim($senha);
             $s_senha = filter_var($senha, FILTER_SANITIZE_STRING);
             if(!filter_var($s_senha,FILTER_VALIDATE_REGEXP, array(
                 "options"=>array("regexp"=>"/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{5,}$/")))){
-                $data= ['sucesso'=>false, 'msg'=>'senha invalida'];
+                $data= ['sucesso'=>false, 'msg'=>$s_endereco];
                 echo json_encode($data);
                 exit();
             }
