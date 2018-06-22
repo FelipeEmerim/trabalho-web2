bea28be9d7e43cee79bbf44bf9b4ee8403600557
Carinaemerim <carinaemerim2@gmail.com>
2018-06-22 09:42:25 -0300

    Add files via upload

--- faleConosco.php
+++ faleConosco.php
@@ -3,7 +3,6 @@
 
 <head>
 
-
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
@@ -22,86 +21,20 @@
     <style>
         @import url('https://fonts.googleapis.com/css?family=MedievalSharp');
 
-        #form{
-            font-family: 'MedievalSharp', cursive;
-            color: #a07e04;
-            margin-left: 15px;
-            border: 1px solid #856404;
-            margin-top: 20px;
-            width: 1108px;
-            height: 450px;
-        }
-
-        #foot{
-            height: 80px;
-            margin-top: 20px;
-
-        }
-
-        #enviar{
-
-            background: #393939;
-            border:none;
-            color: #856404;
-        }
-
-         #enviar:hover{
-            color: red ;
-        }
-
 
     </style>
 </head>
 
 <body>
+    <div id="targetdiv">
+    </div>
 
 <!-- Navigation -->
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-    <div class="container" id="container">
 
-        <a class="navbar-brand" href="#" id="logo"> Ocarina </a>
-
-        <div id="busca">
-            <input type="text" id="txtBusca" placeholder="Buscar"/>
-            <input type="submit" name="buscar" value="Go" id="buscar">
-        </div>
-
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="index.php" id="home"> Home <span class="sr-only">(current)</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="login.php" id="login">Login</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#" id="ver"> Ver Carrinho</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="faleConosco.php" id="fale">Fale Conosco</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
 
 <!-- Page Content -->
 <div class="container">
-
-     <div class="dropdown">
-        <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown" id="button"> Categorias
-            <span class="caret"></span></button>
-        <ul class="dropdown-menu" id="itens">
-            <li><a href="#">Poções</a></li>
-            <li><a href="#">Força</a></li>
-            <li><a href="#">Proteção</a></li>
-        </ul>
-    </div>
 
     <!-- Jumbotron Header -->
     <header class="jumbotron my-4" id="retangulo">
@@ -151,6 +84,7 @@
 <!-- Bootstrap core JavaScript -->
 <script src="startbootstrap-heroic-features-gh-pages/vendor/jquery/jquery.min.js"></script>
 <script src="startbootstrap-heroic-features-gh-pages/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script>$('#targetdiv').load('staticTop.html');</script>
 
 </body>
 
