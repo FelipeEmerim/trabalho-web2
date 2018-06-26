4964823ee7d8b0680497f3545a3138ce26feb71e
FelipeEmerim <lip.emerim@gmail.com>
2018-06-26 01:12:57 -0300

    criada pagina de gerenciamento de usuarios

---
+++ usuarios.php
@@ -0,0 +1,104 @@
+<?php
+    session_start();
+    if(!(isset($_SESSION['usuario']))|| $_SESSION['admin'] == 0){
+        header('Location: index.php');
+    }
+?>
+
+<!DOCTYPE html>
+
+
+
+<html lang="en">
+
+<head>
+
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title> Ocarina - The Store of Your Journey </title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="startbootstrap-heroic-features-gh-pages/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="startbootstrap-heroic-features-gh-pages/css/heroic-features.css" rel="stylesheet">
+
+    <link rel="stylesheet" type="text/css" href="estilos.css">
+
+
+</head>
+
+<body>
+<div id="targetdiv"></div>
+
+<!-- Page Content -->
+<div class="container">
+
+
+    <!-- Jumbotron Header -->
+    <header class="jumbotron my-4" id="retangulo">
+        <h1 class="display-3"> Bem-Vindo!!!</h1>
+        <p class="lead">Gerencie seus usuarios aqui</p>
+
+    </header>
+    <div id="mensagens"></div>
+    <form class = 'busca' onsubmit="buscaUsuario(document.getElementById('user-busca').value); return false"
+          style="left:400px; margin-top:40px;">
+        <input type="text" id="user-busca" placeholder="Buscar"/>
+        <button type="submit" name="buscar" class="buscar">Go</button>
+    </form>
+    <div id="usuarios"></div>
+    <!-- Page Features -->
+
+    <!-- /.row -->
+
+
+</div>
+<!-- /.container -->
+
+<!-- Footer -->
+<footer class="py-5 bg-dark" id="footer">
+    <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; Ocarina 2018</p>
+    </div>
+    <!-- /.container -->
+</footer>
+
+<!-- Bootstrap core JavaScript -->
+<script src="startbootstrap-heroic-features-gh-pages/vendor/jquery/jquery.min.js"></script>
+<script src="startbootstrap-heroic-features-gh-pages/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script>
+    $('#targetdiv').load('staticTop.php');
+    $('#usuarios').load('usuariosTabela.php', {action:'tabela'});
+</script>
+<script src="pesquisa.js"></script>
+
+<script>
+
+    function fecha(element){
+        element.setAttribute("class", "secreto");
+    }
+
+    function remove(cod){
+
+        $.post('usuariosTabela.php', {action: 'remove', codigo: cod}, function(){
+            $('#mensagens').text('Usuario excluido com sucesso').prop('class', 'sucesso');
+            setTimeout(function(){
+                fecha(document.getElementById('mensagens'));
+            }, 4000);
+            $('#usuarios').load('usuariosTabela.php', {action: 'tabela'});
+        });
+    }
+
+    function buscaUsuario(pesq){
+        console.log(pesq);
+        $('#usuarios').load('usuariosTabela.php', {action: 'tabela', pesquisa: pesq});
+    }
+</script>
+
+
+</body>
---
+++ usuariosTabela.php
@@ -0,0 +1,100 @@
+<?php
+
+if(!(isset($_POST['action']))){
+    header('Location:index.php');
+}
+
+$pdo = new PDO('mysql:host=localhost;dbname=loja_virtual', 'root', '');
+
+switch($_POST['action']) {
+
+    case 'tabela':
+
+        if(!(isset($_POST['pesquisa'])) || strlen($_POST['pesquisa']) <= 0) {
+
+            $comando = $pdo->prepare('SELECT * FROM cadastros');
+            $comando->execute();
+            $data = $comando->fetchAll(PDO::FETCH_ASSOC);
+        }else{
+            $pesquisa = strtolower(trim($_POST['pesquisa']));
+
+            $comando = $pdo->prepare('SELECT * FROM cadastros WHERE LOWER(nome) LIKE ? OR LOWER(nome) LIKE ? 
+            OR LOWER(nome) LIKE ? OR LOWER(sexo) LIKE  ? OR LOWER(data) LIKE ? OR LOWER(email) LIKE ? OR 
+            LOWER(endereco) LIKE ?');
+
+            $comando->execute(array($pesquisa, "$pesquisa%", "% $pesquisa%", $pesquisa, $pesquisa, $pesquisa, $pesquisa));
+            $data = $comando->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        ?>
+
+        <head>
+            <style>
+                @import url('https://fonts.googleapis.com/css?family=MedievalSharp');
+
+                #tabela {
+                    margin-top: 40px;
+                    color: #a07e04;
+                    font-family: 'MedievalSharp', cursive;
+
+                }
+
+            </style>
+        </head>
+
+        <!-- Page Features -->
+        <div class="row text-center">
+            <table border="1" id="tabela">
+
+                <?php
+                if (count($data) > 0):
+                    ?>
+                    <tr>
+                        <th width="285"> Nome</th>
+                        <th width="285"> Email</th>
+                        <th width="285"> Endereco</th>
+                        <th width="30">Sexo</th>
+                        <th width="285">Data de nascimento</th>
+                        <th width="285"> Remover Usuario</th>
+
+                    </tr>
+                    <?php
+                    foreach ($data as $usuario):?>
+
+                        <tr>
+                            <td width="285"><?= htmlspecialchars($usuario['nome']) ?></td>
+                            <td width="285"><?= htmlspecialchars($usuario['email']) ?></td>
+                            <td width="285"><?= htmlspecialchars($usuario['endereco']) ?></td>
+                            <td width="30"><?= htmlspecialchars($usuario['sexo']) ?></td>
+                            <td width="285"><?= htmlspecialchars($usuario['data']) ?></td>
+                            <td>
+                                <button class="btn btn-danger"
+                                        onclick="remove('<?= htmlspecialchars($usuario['codigo']) ?>')">X
+                                </button>
+                            </td>
+
+
+                        </tr>
+
+                    <?php
+
+                    endforeach;
+
+                else:
+                    ?>
+                    <tr>Nenhum usuario encontrado</tr>
+                <?php endif; ?>
+            </table>
+
+        </div>
+
+        <?php break;
+
+    case 'remove':
+        $comando = $pdo->prepare('DELETE FROM cadastros WHERE codigo = :codigo');
+        $comando->bindValue(':codigo', intval($_POST['codigo']), PDO::PARAM_INT);
+        $comando->execute();
+        exit();
+}
+
+
